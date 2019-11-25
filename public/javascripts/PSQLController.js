6e94b9eb277b696ac2ee92ad6e9e3bde17a4372f
Vijay NV <19.nvv.11@gmail.com>
2019-11-24 20:42:34 -0700

    added login API

--- public/javascripts/PSQLController.js
+++ public/javascripts/PSQLController.js
@@ -3,3 +3,45 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
+
+ping = async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test');
+      const results = { 'results': (result) ? result.rows : null};
+      res.send( results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  }
+
+login = async (req, res) => {
+	const checklogin = 'SELECT * from users WHERE users.name=$1'
+	const getprofile = 'SELECT * from user_profile WHERE user_profile.name=$1'
+	const values = [res.body.user]
+	output = {}
+	try {
+		const client = await pool.connect()
+		const results = await pool.query(checklogin, values)
+
+		if ( results["pass"] == req.body.pass ):
+			output["result"] = "success"
+			output["code"] = 200
+			const profile = await pool.query(text, values)
+			output["profile"] = profile
+		else:
+			output["result"] = "failure"
+			output["code"] = 400
+		res.send(output)
+		client.release()
+	} catch (err) {
+		console.error(err);
+		res.send("Error " + err);
+	}
+}
+module.exports = {
+	"ping": ping,
+	"login": login
+};
--- routes/psql.js
+++ routes/psql.js
@@ -1,26 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var psqlController = require('../public/javascripts/PSQLController');
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
 /* GET listing. */
-router.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test');
-      const results = { 'results': (result) ? result.rows : null};
-      res.send( results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  });
-
+router.get('/', psqlController.ping);
+router.post('/login', psqlController.login)
 /* GET listing. 
 router.get('/users/', psqlController.getList);
 router.post('/login/:id', psqlController.login);
