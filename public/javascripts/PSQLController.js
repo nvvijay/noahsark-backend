0d3f04ac3246e73be8b078407ef7cbc9c5af54cb
Vijay NV <19.nvv.11@gmail.com>
2019-11-25 12:41:27 -0700

    added method to PUT profile

--- public/javascripts/PSQLController.js
+++ public/javascripts/PSQLController.js
@@ -20,20 +20,41 @@ ping = async (req, res) => {
 login = async (req, res) => {
 	const checklogin = 'SELECT * from users WHERE users.name=$1'
 	const getprofile = 'SELECT * from user_profile WHERE user_profile.name=$1'
-	const values = [res.body.user]
+	const values = [req.body.user]
 	output = {}
 	try {
 		const client = await pool.connect()
 		const results = await pool.query(checklogin, values)
 
-		if ( results["pass"] == req.body.pass ):
+		if ( results["pass"] == req.body.pass ){
 			output["result"] = "success"
 			output["code"] = 200
 			const profile = await pool.query(text, values)
 			output["profile"] = profile
-		else:
+		} else {
 			output["result"] = "failure"
 			output["code"] = 400
+		}
+		res.send(output)
+		client.release()
+	} catch (err) {
+		console.error(err);
+		res.send("Error " + err);
+	}
+}
+
+addProfile = async (req, res) => {
+	const addQuery = 'Insert into userprofiles(name, age, phno, address, city, state, emergency, bloodgrp) values($1, $2, $3, $3, $4, $5, $6, $7, $8)'
+	const values = [req.body.name, req.body.age, req.body.phno, req.body.addr, req.body.city, req.body.state, req.body.emergency, req.body.bloodgrp]
+
+	output = {}
+	try {
+		const client = await pool.connect()
+		const results = await pool.query(checklogin, values)
+
+		output["result"] = "success"
+		output["code"] = 200
+		
 		res.send(output)
 		client.release()
 	} catch (err) {
