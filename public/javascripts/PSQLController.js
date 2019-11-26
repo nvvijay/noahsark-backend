e6045329c3169aef56ea74f1665da4640d43458b
Vijay NV <19.nvv.11@gmail.com>
2019-11-25 22:22:15 -0700

    added password handling

--- public/javascripts/PSQLController.js
+++ public/javascripts/PSQLController.js
@@ -21,7 +21,6 @@ login = async (req, res) => {
 	const checklogin = 'SELECT * from users WHERE users.name=$1'
 	const getprofile = 'SELECT * from user_profile WHERE user_profile.name=$1'
 	const values = [req.body.user]
-	console.log("trying to login user:", values)
 	output = {}
 	try {
 		const client = await pool.connect()
@@ -30,12 +29,11 @@ login = async (req, res) => {
 			output["result"] = "success"
 			output["code"] = 200
 			const profile = await pool.query(getprofile, values)
-			output["profile"] = profile
+			output["profile"] = profile.rows
 		} else {
 			output["result"] = "failure"
 			output["code"] = 400
 		}
-		console.log("output of api", output)
 		res.send(output)
 		client.release()
 	} catch (err) {
@@ -50,7 +48,7 @@ login = async (req, res) => {
 getProfile = async (req, res) => {
 	const getprofile = 'SELECT * from user_profile WHERE user_profile.name=$1'
 	const values = [req.query.user]
-
+	console.log("getting profile for :", values)
 	output = {}
 	try {
 		const client = await pool.connect()
@@ -59,6 +57,7 @@ getProfile = async (req, res) => {
 		output["code"] = 200
 		output["profile"] = results.rows
 		res.send(output)
+		console.log("result of query :", results)
 		client.release()
 	} catch (err) {
 		console.error(err);
@@ -71,13 +70,19 @@ getProfile = async (req, res) => {
 
 addProfile = async (req, res) => {
 	const addQuery = 'insert into user_profile (name, age, phno, address, city, state, emergency, bloodgrp) values ($1, $2, $3, $4, $5, $6, $7, $8)'
+	const addUser = 'insert into users (name, pass) values ($1, $2)'
 	const values = [req.body.name, req.body.age, req.body.phno, req.body.addr, req.body.state, req.body.city, req.body.emergency, req.body.bloodgrp]
+	const values2 = [req.body.name, req.body.passwd]
 	console.log("trying to add profile with valuesL", values)
 	output = {}
 	try {
 		const client = await pool.connect()
 		const results = await pool.query(addQuery, values)
 		console.log("result fo query:", results)
+
+		const usr_result = await pool.query(addUser, values2)
+		console.log("result of usr query:", usr_result)
+
 		output["result"] = "success"
 		output["code"] = 200
 		
